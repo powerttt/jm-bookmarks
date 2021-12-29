fae8bd4a99d1eb1134bf71690d106fa74041c421
powertn <tongning@xiadanba.vip>
2021-12-29 23:35:04 +0800

    去除冗余方法

--- src/renderer/src/store/modules/bookmarks.store.ts
+++ src/renderer/src/store/modules/bookmarks.store.ts
@@ -27,37 +27,6 @@ export const useBookmarksStore = defineStore({
         async setTree(tree: BookMarksItem[]) {
             this.bookmarksTree = tree
         },
-        /**
-         * 加载树
-         * @param $db nedb
-         */
-        async loadTree($db: JmDatastore) {
-            $db.bookmarks.find({}, (err: any, data = []) => {
-                console.log(" bookmarks.store 加载树", data)
-                this.bookmarksTree = bookmarksArray2Tree(data)
-            })
-        },
-        /**
-         * 修改书签
-         * @param $db nedb
-         * @param _bookmarksItem 修改的数据对象
-         * @param reloadTreeFlag 是否重新加载树
-         */
-        uploadBookmarks($db: JmDatastore, _bookmarksItem: BookMarksItem, reloadTreeFlag: boolean) {
-            // 写入数据库，重新刷新tree
-            $db.bookmarks.update(
-                { uuid: _bookmarksItem.uuid },
-                { $set: _bookmarksItem },
-                {},
-                (err: any, numReplaced: number) => {
-                    console.log("err ", err)
-                    console.log("numReplaced", numReplaced)
-                    if (numReplaced != 0 && reloadTreeFlag) {
-                        // 重新加载树
-                        this.localStorage($db)
-                    }
-                })
-        }
     },
 })
 // Need to be used outside the setup
